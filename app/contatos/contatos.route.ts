d3bdcb87b401edc68e66d48e1c0814123aa84b6d
MMonteiro <56174460+MMonteiro-21801093@users.noreply.github.com>
2021-12-11 17:20:51 +0000

    Delete app directory

--- app/contatos/contatos.route.ts
+++
@@ -1,7 +0,0 @@
-import { Route } from '@angular/router';
-import {ContatosComponent} from './contatos.component';
-
-export const CONTATOS_ROUTE: Route = {
-  path: '',
-  component: ContatosComponent
-};
